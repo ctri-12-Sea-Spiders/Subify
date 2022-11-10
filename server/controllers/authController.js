2c0867356f43f6b5f204f3029f79ad4bb19ac565
Brandon Miller <Brandon@MacBook-Pro.local>
2022-11-10 08:56:32 -0800

    added bcrypt to hash passwords, located in usersController and authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,38 +1,36 @@
 const db = require('../model/subifyModel');
-
+const bcrypt = require('bcryptjs');
 const AuthController = {};
 
 //Controller function which serves to verify the user
 AuthController.verifyUser = (req, res, next) => {
-
   const { username, password } = req.body;
-  const queryString = 'SELECT * FROM WHERE username = $1 AND password = $2 RETURNING*;';
-  const params = [username, password];
+  const queryString = 'SELECT * FROM public.users WHERE username = $1;';
+  const params = [username];
 
   db.query(queryString, params)
-    .then(result => {
-      if(result.rows) {
-        //Console log for debugging purposes
-        console.log(result.rows[0]);
-
-        //Check to see if a valid match was found
-        if(result.rows.length > 0)
-          res.locals.username = {username: result.rows[0].username};
-        else
-          res.locals.username = {};
-
-        return next();
+    .then((result) => {
+      if (result.rows[0]) {
+        bcrypt
+          .compare(password, result.rows[0].password)
+          .then((match) => {
+            console.log(match);
+            if (match === false) res.locals.username = {};
+            else res.locals.username = { username: result.rows[0].username };
+            return next();
+          })
+          .catch((err) => next('Error in verify Bcrypt', err));
       }
     })
-    .catch(err => {
+    .catch((err) => {
       return next(err);
-    }); 
+    });
 };
 
 //Controller function to set a new cookie
 AuthController.setCookie = (req, res, next) => {
   //Check to see if a valid login request was found by verifyUser
-  if(res.locals.username.username) {
+  if (res.locals.username.username) {
     res.cookie('token', res.locals.username.username);
   }
   return next();
